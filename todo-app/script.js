c676c85e0e996025deaa65e29d5450de6a810cee
stephipie <stephi.pietschmann@gmail.com>
2024-12-18 17:04:25 +0100

    create a persisting feature to todo-app

--- todo-app/script.js
+++ todo-app/script.js
@@ -4,8 +4,9 @@ const todoList = document.getElementById("todo-list");
 
 let listItem = [];
 
-todoForm.addEventListener("submit", function submitForm(event){
+todoForm.addEventListener("submit", function submitForm(event) {
     event.preventDefault();
+    // const taskText = input.value.trim();
     var taskName = todoInput.value;
     taskName.trim();
     // const listItem = document.createElement("li");
@@ -15,56 +16,98 @@ todoForm.addEventListener("submit", function submitForm(event){
         return false;
     };
     // Task added to list
-    addNewTask(taskName);
-    console.log("Formular übertragen");
+    // addNewTask(taskName);
+    // console.log("Formular übertragen");
     // Clear taskName
-    taskName = "";
+    // taskName = "";
     // Select all Input items within the form-element
-    var allInputs = todoForm.querySelectorAll("input");
+    // var allInputs = todoForm.querySelectorAll("input");
     // All Input added from user will be cleared 
-    allInputs.forEach(singleInput => singleInput.value = "");
+    // .forEach(singleInput => singleInput.value = "");
+    const newTask = {
+        id: Date.now(),
+        textInput: taskName,
+        status: false
+    };
+    listItem.push(newTask);
+    // console.log(listItem);
+    taskName = '';
+    showTodos();
 });
 
-function addNewTask(taskForm) {
-    // todoList.innerHTML = '';
-    const taskText = taskForm;
+function showTodos() {
+    todoList.innerHTML = '';
+    // const taskText = taskForm;
     // Id for our task
-    var date = Date.now();
+    // var date = Date.now();
     // Define Object
-    const task = {
-        status: false, 
-        textInput: taskText,
-        id: date
-    };
+    // const task = {
+    //     status: false, 
+    //     textInput: taskText,
+    //     id: date
+    // };
     // Add Object to Array
-    listItem.push(task);
+    // listItem.push(task);
 
     // Add new task to task-list
     listItem.forEach((task) => {
+        const taskListItem = document.createElement("li");
+        
         const checkBox = document.createElement("input");
-        checkBox.setAttribute('type','checkbox');
-        checkBox.setAttribute('aria-label','Aufgabe erledigen');
-
+        checkBox.type = 'checkbox';
+        // checkBox.setAttribute('type','checkbox');
+        checkBox.setAttribute('aria-label', 'Aufgabe erledigen');
+        // showTodos();
+        saveTodos();
+        
         const span = document.createElement("span");
-        span.innerText = task.id + ' ' + task.textInput;
+        // span.innerText = task.id + ' ' + task.textInput;
+        span.innerText = task.textInput;
         
         const button = document.createElement("button");
         button.innerText = "Löschen";
-        button.setAttribute('aria-label','Aufgabe löschen');
-        
-        const taskListItem = document.createElement("li");
-        taskListItem.appendChild(checkBox);
-        taskListItem.appendChild(span);
-        taskListItem.appendChild(button);
-
-        todoList.appendChild(taskListItem);
-
+        button.setAttribute('aria-label', 'Aufgabe löschen');
         // Function of delete-button
         button.addEventListener('click', () => {
             const actualListItem = button.parentElement;
             todoList.removeChild(actualListItem);
+            listItem.pop(task.id);
+            showTodos();
+            saveTodos();
         });
-
-    listItem.pop(task.id);
+        
+        taskListItem.appendChild(checkBox);
+        taskListItem.appendChild(span);
+        taskListItem.appendChild(button);
+        todoList.appendChild(taskListItem);
+        // listItem.pop(task.id);
     });
+    saveTodos();
 };
+
+// Create function to save todo´s
+function saveTodos() {
+    if (listItem.length === 0) {
+        return;
+    };
+    const localStorageTodos = JSON.stringify(listItem);
+    // console.log(localStorageTodos)
+    localStorage.setItem("todos", localStorageTodos);
+    // console.log(localStorage.getItem("todos"))
+};
+// saveTodos()
+// Create load function 
+function loadTodos() {
+    const existingTodos = localStorage.getItem("todos");
+    if (!existingTodos) {
+        listItem = [];
+    }
+    // console.log(typeof (existingTodos));
+    const parse = JSON.parse(existingTodos);
+    listItem = parse;
+    // console.log(listItem)
+    // console.log(loadedTodosFromLocalStorage)
+};
+loadTodos();
+showTodos();
+
